8cfd40c74bc0d684302dd548209dbe3cb0561bc4
Giulia <89267265+agiulsz@users.noreply.github.com>
2021-10-04 13:35:56 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="estilo.css"> 
+  <title>Document</title>
+</head>
+
+<body>
+  
+  <div>
+  <h1>PLAYLIST</h1>
+  <h2>Brasileirinhos by Giulia</H2>
+  <p>1. São Gonça - Seu Jorge<br>2. Cara Valente - Maria Rita<br>3. Melhor Lugar - Jorge Vercillo<br>
+  4. A Sós - Arlindo Cruz<br>5. Ela é Carioca - João Gilberto<br>6. Eu te amo - Chico Buarque<br>
+  7. Linha do Equador - Rael<br>8. Ordem Natural Das Coisa - Emicida<br>9. Talvez - Grupo Revelação<br>
+  10. Tudo Azul - Circuladô de Fulô</p>
+</div>
+
+</body>
+</html>
